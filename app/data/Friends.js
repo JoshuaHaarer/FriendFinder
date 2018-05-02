d9080e6ae95336f2343511707d0338cc366b463d
joshua <joshuahaarer86@gmail.com>
2018-05-01 18:35:50 -0700

    small support files

---
+++ app/data/Friends.js
@@ -0,0 +1,8 @@
+var friendArray = [
+    {
+      friendName: "",
+      friendImage: ""
+    }
+  ];
+
+module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require ("express");
+var bodyParser = require ("body-parser");
+var path = require ("path");
+
+var app = express(); 
+var port = process.env.PORT || 3000; 
